76126d8d35c4b5a814befa3af87539406cce5db2
Renan Miranda <renanlmiranda@gmail.com>
2020-11-26 02:01:34 -0300

    alert rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': 0,
     'no-console': 0,
     'no-implicit-globals': 0,
+    'no-alert': 0,
     // 'no-unused-expressions': 0,
   },
 };
